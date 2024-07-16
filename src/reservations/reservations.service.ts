966b0f9a0b849d2ec81a61ac08cf87ab9cb8b746
Ali Saleh <alifadisaleh99@gmail.com>
2024-07-16 18:11:04 +0300

    Possibility Check

--- src/reservations/reservations.service.ts
+++ src/reservations/reservations.service.ts
@@ -1,6 +1,6 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { LessThan, MoreThan, Repository } from 'typeorm';
 import { Reservation } from './entity/reservations.entity';
 import { createReservationDto } from './dto/reservations.dto';
 import { RealEstatesService } from 'src/real-estate/real-estates.service';
@@ -21,6 +21,11 @@ export class ReservationsService {
         const real_estate = await this.realEstateService.findRealEstate(data.real_estate_id)
         if(!real_estate)
             throw new BadRequestException('Real state not found')
+
+        const status = await this.possibilityCheck(data)
+        if(!status)
+            throw new BadRequestException('Cannot reservation right now,the real estate is reserved!')
+        
         const user = await this.userService.findMe(user_id)
 
         if(data.start_date > data.end_date)
@@ -39,12 +44,55 @@ export class ReservationsService {
         }
         const cost = price_per_day * reservation_days
 
-        return this.reservationRepository.create({
+        return this.reservationRepository.save({
             user,
             real_estate,
             cost,
             start_date: data.start_date,
             end_date: data.end_date
         })
+    }
+
+    
+    possibilityCheck = async (data: createReservationDto) => {
+        let status = true
+
+        const real_estate_reservations =await this.reservationRepository.find({
+            order: {
+                start_date: "ASC"
+            }
+        })
+
+        const previous_reservation = await this.reservationRepository.findOne({
+            order: {
+                start_date: "DESC"
+            },
+            where: {
+                start_date: LessThan(data.start_date)
+            }
+        })
+
+        const subsequent_reservation = await this.reservationRepository.findOne({
+            order: {
+                start_date: "ASC"
+            },
+            where: {
+                start_date: MoreThan(data.start_date)
+            }
+        })
+
+        if(!real_estate_reservations){
+            status = true
+        } else if (real_estate_reservations.length == 1) {
+            if(data.start_date > real_estate_reservations[0].end_date || data.end_date < real_estate_reservations[0].start_date)
+                status = true
+            else
+                status = false
+        } else if (real_estate_reservations.length > 1) {
+            if(data.end_date > subsequent_reservation.start_date || data.start_date < previous_reservation.end_date)
+                status = false
+        }
+
+        return status
     }
 }
